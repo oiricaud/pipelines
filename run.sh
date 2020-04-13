59839fe9c48dc4502bf2c4576f7ce0e00d63f0e7
oscar-ricaud <Oscar.Ricaud@ibm.com>
2020-04-13 11:55:33 -0500

    uploading pipelines

--- run.sh
+++ run.sh
@@ -117,7 +117,7 @@ update_kabanero_cr() {
   # define variables
   name_of_pipeline="oscar-custom-pipelines"
   pipeline_to_update=\"${name_of_pipeline}\"
-  new_url="https://github.com/oiricaud/pipelines/releases/download/v40.0/default-kabanero-pipelines.tar.gz"
+  new_url="https://github.com/oiricaud/pipelines/releases/download/v41.0/default-kabanero-pipelines.tar.gz"
   get_sha=$(shasum -a 256 ./ci/assets/default-kabanero-pipelines.tar.gz | grep -Eo '^[^ ]+' )
 
   # add double quotes to the sha256
